ec3df54463ab7d21dfc7f17530d11122f82523d2
Go-youngho <ksc787833@naver.com>
2021-05-26 16:48:53 +0900

    first commit

---
+++ test_predict.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import glob, numpy as np
+from keras.models import load_model
+
+caltech_dir = "./test_src"
+image_w = 64
+image_h = 64
+
+pixels = image_h * image_w * 3
+
+X = []
+filenames = []
+files = glob.glob(caltech_dir+"/*.*")
+for i, f in enumerate(files):
+    img = Image.open(f)
+    img = img.convert("RGB")
+    img = img.resize((image_w, image_h))
+    data = np.asarray(img)
+    filenames.append(f)
+    X.append(data)
+
+X = np.array(X)
+model3 = load_model('./model/multi_img_classification.model')
+
+prediction = model3.predict(X)
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+cnt = 0
+
+#이 비교는 그냥 파일들이 있으면 해당 파일과 비교.
+#카테고리와 함께 비교해서 진행하는 것은 _4 파일.
+for i in prediction:
+    pre_ans = i.argmax()  # 예측 레이블
+    print(i)
+    print(pre_ans)
+    pre_ans_str = ''
+    if pre_ans == 0: pre_ans_str = "head"
+    elif pre_ans == 1: pre_ans_str = "noise"
+    else: pre_ans_str = "finger"
+    if i[0] >= 0.8 : print("해당 "+filenames[cnt].split("\\")[1]+"이미지는 "+pre_ans_str+"로 추정됩니다.")
+    if i[1] >= 0.8: print("해당 "+filenames[cnt].split("\\")[1]+"이미지는 "+pre_ans_str+"으로 추정됩니다.")
+    if i[2] >= 0.8: print("해당 "+filenames[cnt].split("\\")[1]+"이미지는 "+pre_ans_str+"로 추정됩니다.")
+    cnt += 1
